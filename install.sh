fde1f7f0defa58f78c94a42b552cc8cee75254ea
Andrew Lee <leeas@stolaf.edu>
2017-04-21 15:27:01 -0500

    modded

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
 #Script to install coreos on macmini
-
+apt-get install curl gawk
 curl --remote-name https://raw.githubusercontent.com/coreos/init/master/bin/coreos-install
 chmod u+x coreos-install
 
-curl --remote-name https://raw.githubusercontent.com/des4maisons/coreos-macmini-bootstrap/master/cloud-config.yml
+curl --remote-name https://raw.githubusercontent.com/gilgameshskytrooper/coreos-bootstrap/master/cloud-config.yml
 
 #./coreos-install -d /dev/sda -c cloud-config.yml -C stable
 ./coreos-install -d /dev/sda -c ignition.json
 
-rm -rf ignition.json  coreos-install
+rm -rf cloud-config.yml  coreos-install
